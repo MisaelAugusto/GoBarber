de5666e71afa8da22eef245a999253d4fe924545
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-08-13 11:23:21 -0300

    Set postgres database and typeorm up

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import UsersRouter from './users.routes';
 import AppointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
+routes.use('/users', UsersRouter);
 routes.use('/appointments', AppointmentsRouter);
 
 export default routes;
