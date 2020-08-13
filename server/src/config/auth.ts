fcbb13d44cdf6ae1694200f759fdb05d7fa6d744
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-08-13 12:20:06 -0300

    Add user authentication

---
+++ server/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '89dbb65161c3fe13ad5478f19e2b355d',
+    expiresIn: '1d'
+  }
+};
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UsersRouter from './users.routes';
+import SessionsRouter from './sessions.routes';
 import AppointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
 routes.use('/users', UsersRouter);
+routes.use('/sessions', SessionsRouter);
 routes.use('/appointments', AppointmentsRouter);
 
 export default routes;
